b4a124007937408d553c87da3b6e4d97758e2842
Chris Cavell <cdcavell@gmail.com>
2025-03-16 12:25:36 -0500

    Initial Development

---
+++ Source/Web/AI.Chatbot/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.HttpOverrides;
+using Serilog;
+
+namespace AI.Chatbot.Extensions
+{
+    internal static class WebApplicationExtensions
+    {
+        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Host.UseSerilog((ctx, lc) => lc
+                .ReadFrom.Configuration(ctx.Configuration));
+
+            builder.Services.AddHttpContextAccessor();
+            builder.Services.AddMvc();
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddResponseCaching();
+
+            return builder.Build();
+        }
+
+        public static WebApplication ConfigurePipeline(this WebApplication app)
+        {
+            app.UseSerilogRequestLogging();
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            app.UseResponseCaching();
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+            return app;
+        }
+    }
+}
---
+++ Source/Web/AI.Chatbot/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AI.Chatbot.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
